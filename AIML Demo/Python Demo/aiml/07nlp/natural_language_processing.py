6a46667870a01072f2e8e1a0492869a0f3cc3a9d
Jimmy Joy <35603896+jimmypjoy@users.noreply.github.com>
2020-07-01 12:29:14 -0400

    aiml

--- AIML Demo/Python Demo/aiml/07nlp/natural_language_processing.py
+++ AIML Demo/Python Demo/aiml/07nlp/natural_language_processing.py
@@ -46,3 +46,5 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test,y_pred))
